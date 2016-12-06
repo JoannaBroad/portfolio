d7c0864eb3dda84c1dcb01e661773dafdd2d6edd
joanna broad <joanna.broad93@gmail.com>
2016-12-06 15:14:42 +0000

    added debs cards

--- js/work.js
+++ js/work.js
@@ -3,9 +3,9 @@ var works = [{
         href: "https://jo-calculator.bitballoon.com",
         pic: "img/cal.png"
     }, {
-        title: "coming soon",
-        href: "#work",
-        pic: "img/coming.jpeg"
+        title: "Debs cards",
+        href: "https://debs-cards.herokuapp.com/",
+        pic: "img/debs-cards.png"
     }, {
         title: "coming soon",
         href: "#work",
